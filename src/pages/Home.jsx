49886407d41bf533c06d98ec48ca1f91604b9f8d
frst1009 <fttkpsttkp@gmail.com>
2024-01-22 23:55:05 +0400

    changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import './home.css';
-import Footer from '../components/Footer';
 
 const Home = () => {
-  return (
-    <div className="hero">
+  return (<>
+   <div className="hero">
       <h1>Welcome to Your Blog</h1>
       <p>Discover amazing content and more...</p>
       <button>Explore Blogs</button>
-      <Footer/>
     </div>
+      </>
+   
   );
 };
 
