47e445de71c987864d4a3a5f81b3116c2dbe72df
frst1009 <fttkpsttkp@gmail.com>
2024-02-16 11:01:27 +0400

    added responsiveness

--- blog/src/components/Footer.jsx
+++ blog/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Footer() {
   return (
-    <div className="text-pink-50 w-full h-8 text-center text-xs bg-amber-600/80 p-2 ">
+    <div className="text-pink-50 w-full h-6 text-center text-s bg-amber-600/80 py-1 md:text-xs md:h-8 md:py-2">
       <p>&copy; 2024 Your Blog. All rights reserved.</p>
     </div>
   );
--- blog/src/pages/Home.jsx
+++ blog/src/pages/Home.jsx
@@ -4,17 +4,17 @@ function Home() {
   
   return (
     <>
-      <div className="text-center bg-[url(./assets/2.gif)] bg-cover bg-center bg-no-repeat py-20 -mt-28 flex-grow flex justify-center align-center flex-col text-pink-50">
-        <h1 className="mb-3 text-4xl font-bold">
+      <div className="text-center bg-[url(./assets/2.gif)] bg-cover bg-center bg-no-repeat -mt-20 flex-grow flex justify-center align-center flex-col text-pink-50 ">
+        <h1 className="mb-3 text-2xl font-bold md:text-3xl">
           Welcome to <span className="italic">VieW</span>
         </h1>
-        <p className="mb-3 font-medium text-2xl">
+        <p className="mb-3 text-lg font-medium md:text-xl">
           Discover amazing content and more...
         </p>
         <div>
           <button
             onClick={() => (window.location.href = "/blogs")}
-            className="button py-4 px-8 m-auto border-2 ease-in duration-500 border-gray-300 rounded-xl hover:border-2 hover:bg-purple-900/50 hover:border-amber-600 hover:scale-110 hover:shadow-indigo-800/50 hover:shadow-md hover:font-bold"
+            className="button text-sm py-3 px-6 m-auto border-2 ease-in duration-500 border-gray-300 rounded-xl hover:border-2 hover:bg-purple-900/50 hover:border-amber-600 hover:scale-110 hover:shadow-indigo-800/50 hover:shadow-md hover:font-bold md:py-4 md:px-8 md:text-base"
           >
             Explore Blogs
           </button>
